108677e9308ee87f27cbabd847124508ae83b5fd
Lucas Almeida <developer.lucas24@gmail.com>
2023-03-23 20:57:28 +0100

    feat: create OrderEvent for all order events

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/event/OrderEvent.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.domain.event;
+
+import com.food.ordering.system.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public abstract class OrderEvent implements DomainEvent<Order> {
+
+    private final Order order;
+    private final ZonedDateTime createdAt;
+
+    public OrderEvent(Order order, ZonedDateTime createdAt) {
+        this.order = order;
+        this.createdAt = createdAt;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/event/OrderPaidEvent.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.domain.event;
+
+import com.food.ordering.system.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderPaidEvent extends OrderEvent {
+
+    public OrderPaidEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
