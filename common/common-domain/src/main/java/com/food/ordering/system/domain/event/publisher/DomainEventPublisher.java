aaccee496bf841854188aff0acdd2bb581c87e5e
Lucas Almeida <developer.lucas24@gmail.com>
2023-03-24 23:41:58 +0100

    feat: create domain event publisher

--- common/common-domain/src/main/java/com/food/ordering/system/domain/event/publisher/DomainEventPublisher.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/event/publisher/DomainEventPublisher.java
@@ -2,7 +2,7 @@ package com.food.ordering.system.domain.event.publisher;
 
 import com.food.ordering.system.domain.event.DomainEvent;
 
-public interface DomainEventPublisher <T extends DomainEvent>{
+public interface DomainEventPublisher<T extends DomainEvent> {
 
     void publish(T domainEvent);
 }
