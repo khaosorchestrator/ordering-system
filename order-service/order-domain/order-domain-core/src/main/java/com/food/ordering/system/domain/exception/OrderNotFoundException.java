bb496f7fa6648ed66801a371a13472325b456313
Lucas Almeida <developer.lucas24@gmail.com>
2023-03-26 22:35:43 +0100

    feat: add OrderNotFoundException

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/exception/OrderNotFoundException.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.domain.exception;
+
+public class OrderNotFoundException extends DomainException {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public OrderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
