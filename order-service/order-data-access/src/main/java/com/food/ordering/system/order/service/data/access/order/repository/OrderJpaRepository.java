8cf176eb4bacbb93d633eefbb6d69c6f443b414d
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-04 21:23:16 +0100

    feat: add order entity jpa repository

---
+++ order-service/order-data-access/src/main/java/com/food/ordering/system/order/service/data/access/order/repository/OrderJpaRepository.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.order.service.data.access.order.repository;
+
+import com.food.ordering.system.order.service.data.access.order.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface OrderJpaRepository extends JpaRepository<OrderEntity, UUID> {
+
+    Optional<OrderEntity> findByTrackingId(UUID trackingId);
+}
