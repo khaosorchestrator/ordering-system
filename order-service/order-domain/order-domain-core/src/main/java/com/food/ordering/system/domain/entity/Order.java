ad8736daeb97d9f0a6be82c12e510562934e42c2
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-04 21:23:34 +0100

    feat: add order data access mapper

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Order.java
@@ -17,6 +17,7 @@ public class Order extends AggregateRoot<OrderId> {
     private TrackingId trackingId;
     private OrderStatus orderStatus;
     private List<String> failureMessages;
+    public static final String FAILURE_MESSAGE_DELIMITER = ",";
 
     private Order(Builder builder) {
         super.setId(builder.orderId);
