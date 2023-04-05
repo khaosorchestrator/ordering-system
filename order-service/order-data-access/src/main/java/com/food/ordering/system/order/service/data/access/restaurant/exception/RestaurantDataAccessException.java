9009e0a9cd293aab93b2b878cebdf3bee32ca274
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-05 22:14:50 +0100

    feat: add restaurant data access mapper

---
+++ order-service/order-data-access/src/main/java/com/food/ordering/system/order/service/data/access/restaurant/exception/RestaurantDataAccessException.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.order.service.data.access.restaurant.exception;
+
+public class RestaurantDataAccessException extends RuntimeException {
+
+    public RestaurantDataAccessException(String message) {
+        super(message);
+    }
+}
