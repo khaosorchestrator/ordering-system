e21023430c2c699e04a593f1f13663621f51176d
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-05 22:13:08 +0100

    feat: add customer entity

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Customer.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Customer.java
@@ -3,4 +3,11 @@ package com.food.ordering.system.domain.entity;
 import com.food.ordering.system.domain.valueobject.CustomerId;
 
 public class Customer extends AggregateRoot<CustomerId> {
+
+    public Customer() {
+    }
+
+    public Customer(CustomerId customerId) {
+        super.setId(customerId);
+    }
 }
