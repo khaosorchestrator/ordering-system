7275f4971e9ca16f20fb31f62da698f6c06d823f
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-05 22:15:16 +0100

    feat: add restaurant repository adapter

---
+++ order-service/order-data-access/src/main/java/com/food/ordering/system/order/service/data/access/restaurant/adapter/RestaurantRepositoryAdapter.java
@@ -0,0 +1,29 @@
+package com.food.ordering.system.order.service.data.access.restaurant.adapter;
+
+import com.food.ordering.system.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.data.access.restaurant.entity.RestaurantEntity;
+import com.food.ordering.system.order.service.data.access.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.order.service.data.access.restaurant.repository.RestaurantJpaRepository;
+import com.food.ordering.system.order.service.domain.ports.output.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class RestaurantRepositoryAdapter implements RestaurantRepository {
+
+    private final RestaurantJpaRepository restaurantJpaRepository;
+    private final RestaurantDataAccessMapper restaurantDataAccessMapper;
+
+    @Override
+    public Optional<Restaurant> findRestaurantInformation(Restaurant restaurant) {
+        List<UUID> restaurantProducts = restaurantDataAccessMapper.restaurantToRestaurantProducts(restaurant);
+        Optional<List<RestaurantEntity>> restaurantEntities = restaurantJpaRepository
+                .findByRestaurantIdAndProductIdIn(restaurant.getId().getValue(), restaurantProducts);
+        return restaurantEntities.map(restaurantDataAccessMapper::restaurantEntityToRestaurant);
+    }
+}
