08366b60d147bcc16667b51d77734c19ff6db958
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-11 20:26:56 +0100

    chore: add init-data.sql and init-schema.sql

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', 'beck_alva', 'Beck', 'Alva');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb08', 'lucy_cross', 'Lucy', 'Cross')
