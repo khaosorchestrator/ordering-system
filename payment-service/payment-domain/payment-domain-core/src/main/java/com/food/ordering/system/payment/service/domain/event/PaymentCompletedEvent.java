2451be36e402926febe7ee8acb6be0739d1245c3
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-15 11:39:28 +0100

    feat: add payment event

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/event/PaymentCompletedEvent.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.payment.service.domain.event;
+
+import com.food.ordering.system.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class PaymentCompletedEvent extends PaymentEvent {
+
+    public PaymentCompletedEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
+        super(payment, createdAt, failureMessages);
+    }
+}
