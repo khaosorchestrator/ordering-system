b4a94ea3e3379655de59c80d12540068c8a31ea9
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-17 00:24:04 +0100

    feat: add credit entry data access mapper

---
+++ payment-service/payment-data-access/src/main/java/com/food/ordering/system/payment/service/dataaccess/creditentry/exception/CreditEntryDataAccessException.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.payment.service.dataaccess.creditentry.exception;
+
+public class CreditEntryDataAccessException extends RuntimeException {
+
+    public CreditEntryDataAccessException(String message) {
+        super(message);
+    }
+}
