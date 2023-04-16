7eb12ab6e6bca47840639cc4c2e2f26aa7bca261
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-17 00:22:15 +0100

    feat: add credit history data access mapper

---
+++ payment-service/payment-data-access/src/main/java/com/food/ordering/system/payment/service/dataaccess/credithistory/exception/CreditHistoryDataAccessException.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.payment.service.dataaccess.credithistory.exception;
+
+public class CreditHistoryDataAccessException extends RuntimeException {
+
+    public CreditHistoryDataAccessException(String message) {
+        super(message);
+    }
+}
