57c438599cf57c3e94b124b6d9ef964cfd34464c
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-19 06:11:56 +0100

    feat: add payment container

---
+++ payment-service/payment-container/src/main/resources/init-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb20', 'd215b5f8-0249-4dc5-89a3-51fd148cfb24', 500.00);
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb21', 'd215b5f8-0249-4dc5-89a3-51fd148cfb24', 100.00, 'CREDIT');
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb22', 'd215b5f8-0249-4dc5-89a3-51fd148cfb24', 600.00, 'CREDIT');
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb23', 'd215b5f8-0249-4dc5-89a3-51fd148cfb24', 200.00, 'DEBIT');
+
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', 'd215b5f8-0249-4dc5-89a3-51fd148cfb24', 100.00);
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb25', 'd215b5f8-0249-4dc5-89a3-51fd148cfb24', 100.00, 'CREDIT');
