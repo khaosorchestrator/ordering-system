8ac6c939e21f566987dec8c144be8968573f4046
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-23 22:53:02 +0100

    feat: add restaurant approval request message listener implementation

---
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/ports/input/message/listener/RestaurantApprovalRequestMessageListener.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.restaurant.service.domain.ports.input.message.listener;
+
+import com.food.ordering.system.restaurant.service.domain.dto.RestaurantApprovalRequest;
+
+public interface RestaurantApprovalRequestMessageListener {
+
+    void approveOrder(RestaurantApprovalRequest restaurantApprovalRequest);
+}
