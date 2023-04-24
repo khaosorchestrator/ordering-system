f1f1df0cdb3688092612d3455fba77edd6397daa
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-24 22:41:17 +0100

    feat: add order approval jpa repository

---
+++ restaurant-service/restaurant-data-access/src/main/java/com/ordering/system/restaurant/service/data/access/restaurant/repository/OrderApprovalJpaRepository.java
@@ -0,0 +1,11 @@
+package com.ordering.system.restaurant.service.data.access.restaurant.repository;
+
+import com.ordering.system.restaurant.service.data.access.restaurant.entity.OrderApprovalEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderApprovalJpaRepository extends JpaRepository<OrderApprovalEntity, UUID> {
+}
