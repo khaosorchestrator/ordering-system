726076605ece90455af0df625c4c65969e726358
Lucas Almeida <developer.lucas24@gmail.com>
2023-04-24 22:42:48 +0100

    build: add common data access module

--- order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
+++ order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.food.ordering.system.order.service.data.access")
-@EntityScan(basePackages = "com.food.ordering.system.order.service.data.access")
+@EnableJpaRepositories(basePackages = {"com.food.ordering.system.order.service.data.access", "com.food.ordering.system.data.access"})
+@EntityScan(basePackages = {"com.food.ordering.system.order.service.data.access", "com.food.ordering.system.data.access"})
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
 public class OrderServiceApplication {
 
